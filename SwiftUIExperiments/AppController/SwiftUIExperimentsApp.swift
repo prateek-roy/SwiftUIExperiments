53487b85e9d3c018e339c7010668b70e2498cdcc
Prateek Roy <prateekroy01@yahoo.com>
2020-09-25 18:08:10 +0530

    A simple button animation

--- SwiftUIExperiments/AppController/SwiftUIExperimentsApp.swift
+++ SwiftUIExperiments/AppController/SwiftUIExperimentsApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUIExperimentsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
---
+++ SwiftUIExperiments/HomeView/HomeView.swift
@@ -0,0 +1,47 @@
+//
+//  HomeView.swift
+//  SwiftUIExperiments
+//
+//  Created by Prateek Roy on 25/09/20.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @State private var show = false
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            if show {
+                LabelView()
+                    .transition(.asymmetric(insertion: .scale(scale: 0.5), removal: AnyTransition.opacity.combined(with: .slide)))
+            }
+            Spacer()
+            Button("Animate") {
+                withAnimation(.easeInOut(duration: 1.0)) {
+                    self.show.toggle()
+                }
+                
+            }
+            .padding()
+        }
+        .padding()
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
+
+struct LabelView: View {
+    var body: some View {
+        Text("Hello User")
+            .padding(24)
+            .font(.title)
+            .foregroundColor(.white)
+            .background(RoundedRectangle(cornerRadius: 12).fill(Color.green).shadow(color: .gray, radius: 5, y: 2))
+    }
+}
