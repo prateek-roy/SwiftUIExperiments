d03598d37ea246476ddfde0969ae8e2dbd7b3b19
Prateek Roy <prateekroy01@yahoo.com>
2020-09-25 16:17:06 +0530

    Initial Commit

---
+++ SwiftUIExperiments/SwiftUIExperimentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExperimentsApp.swift
+//  SwiftUIExperiments
+//
+//  Created by Prateek Roy on 25/09/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExperimentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
